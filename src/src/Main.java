e97f12f996aae0c5a1a7166bd38e2d3cc4a7f63c
Koray <koraykilli@posta.mu.edu.tr>
2025-05-09 11:57:13 +0300

    final push

---
+++ src/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // It basically returns same result.
+        // Additional Homework: do the same thing in arrays.
+
+        Stack stack = new StackedLinkedList();
+        stack.push("Hello World");
+        stack.peek();
+        stack.push("Hello 2 World.");
+        stack.peek();
+        stack.pop();
+        stack.peek();
+
+
+
+    }
+}
---
+++ src/src/StackedLinkedList.java
@@ -0,0 +1,36 @@
+public class StackedLinkedList implements Stack{
+
+    StackItem top;
+
+
+    @Override
+    public void push(Object item) {
+        StackItem newBox = new StackItem(item);
+        StackItem previousTop = this.top;
+        this.top = newBox;
+        top.setNextpart(previousTop);
+
+        // Create a box
+        // empty the top
+        // put the top
+
+
+    }
+
+    @Override
+    public Object pop() {
+        Object tempData = top.getData();
+        top = top.getNextpart();
+        return tempData;
+    }
+
+    @Override
+    public void peek() {
+        System.out.println("The top is " + this.top.getData());
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+}
